23f344d6572dbc4c4d24ec6de763a769373c566f
saddamvs <saddam.husain@vectoscalar.com>
2016-06-09 12:28:09 +0530

    Integrating manager

---
+++ Manager.h
@@ -0,0 +1,15 @@
+//
+//  Manager.h
+//  FinalFW
+//
+//  Created by VS-Saddam Husain-MacBookPro on 08/06/16.
+//  Copyright © 2016 VS-Saddam Husain-MacBookPro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Manager : NSObject
+
+
++ (void)myManger;
+@end
---
+++ Manager.m
@@ -0,0 +1,17 @@
+//
+//  Manager.m
+//  FinalFW
+//
+//  Created by VS-Saddam Husain-MacBookPro on 08/06/16.
+//  Copyright © 2016 VS-Saddam Husain-MacBookPro. All rights reserved.
+//
+
+#import "Manager.h"
+
+@implementation Manager
+
++ (void)myManger
+{
+    NSLog(@"Manager");
+}
+@end
